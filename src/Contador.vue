7139305925149db43bbe7ce05030eb64d2f81f7a
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-02-12 15:29:56 -0400

    104. Criar um Componente

---
+++ src/Contador.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="contador">
+        <span>{{ contador }}</span>
+        <button @click="adicionar">+</button>
+        <button @click="subtrair">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        adicionar() {
+            this.contador++
+        },
+        subtrair() {
+            this.contador--
+        }
+    }
+}
+</script>
+
+<style scoped>
+.contador span {
+    height: 30px;
+    padding: 5px 25px;
+    border-bottom: 1px solid #ccc;
+}
+</style>
